abedadaaa297dbc3281158c919c38a2e3bf9400e
quyetint <quyetint@users.noreply.github.com>
2016-11-22 12:14:38 +0700

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
 <head>
 	<title></title>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="include/theme/default/css/style.css">
+	<link rel="stylesheet" type="text/css" href="include/theme/default/css/style.css" />
 </head>
 <body>
